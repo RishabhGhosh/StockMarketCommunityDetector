9d6e0e0aec3190095f799b3d5ee7a82632b9c378
lkyfly0765 <zhongkun@usc.edu>
2023-11-03 14:50:40 -0700

    modified:   ../../prerequisite/install_prerequisite.sh

--- prerequisite/install_prerequisite.sh
+++ prerequisite/install_prerequisite.sh
@@ -16,3 +16,23 @@ echo "yfinance" >> requirements.txt
 
 # Install the required packages
 pip install -r ./requirements.txt
+
+# For the finBERT requirement
+# Define the required Python packages
+packages=(
+    "jupyter>=1.0.0"
+    "pandas>=0.23.4"
+    "numpy>=1.16.3"
+    "nltk"
+    "tqdm"
+    "ipykernel>=5.1.3"
+    "transformers>=4.1.1"
+    "joblib>=0.13.2"
+    "scikit-learn>=0.21.2"
+    "spacy>=2.1.4"
+    "torch>=1.1.0"
+    "textblob"
+)
+
+# Install the packages using pip
+pip install "${packages[@]}"
